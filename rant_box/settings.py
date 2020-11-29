0306cc23377518426ea756f1f7228ad7a388b123
anuraj <anurajguha@gmail.com>
2020-11-29 19:30:45 +0530

    add limit offset pagination

--- rant_box/settings.py
+++ rant_box/settings.py
@@ -87,7 +87,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
 
 # Password validation
