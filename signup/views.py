b993f437e02b5c8fdad2ca83cc281d5bf4dbae96
anuraj <anurajguha@gmail.com>
2020-12-26 12:16:55 +0530

    add log entry for signup views

--- signup/views.py
+++ signup/views.py
@@ -14,13 +14,16 @@ class UserRegistrationView(APIView):
             username = request.data['username']
             password = request.data['password']
             confirm_password = request.data['confirm_password']
-        except KeyError:
+        except KeyError as e:
+            logging.getLogger('root').error(f'KeyError {str(e)}')
             return Response({'error_message': 'Bad request'}, status=status.HTTP_400_BAD_REQUEST)
 
         if password != confirm_password:
+            logging.getLogger('root').error('Passwords do not match')
             return Response({'error_message': 'passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
 
         if User.objects.filter(username=username).exists():
+            logging.getLogger('root').error('Username already exists')
             return Response(
                 {'error_message': 'username already exists. Try with another username.'},
                 status=status.HTTP_400_BAD_REQUEST
@@ -30,10 +33,11 @@ class UserRegistrationView(APIView):
             user = User.objects.create(username=username)
             user.set_password(password)
             user.save()
+            logging.getLogger('root').debug('User created')
             return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
 
         except Exception as e:
-            logging.getLogger('root').error(f'[*] {e}')
+            logging.getLogger('root').error(str(e))
             return Response(
                 {'error_message': 'some error occurred. User creation failed.'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
