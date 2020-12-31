e3c00d6f79b3a852b0a08ee11c2738a52907dad8
anuraj <anurajguha@gmail.com>
2021-01-01 03:11:57 +0530

    change permissions to IsAuthenticated

--- rant_post/views.py
+++ rant_post/views.py
@@ -1,14 +1,14 @@
 from rest_framework import generics
 from .serializers import RantPostSerializer
 from .models import RantPost
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from .permissions import RantPostUpdateDeletePermissions
 
 
 class RantPostListCreateView(generics.ListCreateAPIView):
     serializer_class = RantPostSerializer
     queryset = RantPost.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -17,5 +17,5 @@ class RantPostListCreateView(generics.ListCreateAPIView):
 class RantPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RantPostSerializer
     queryset = RantPost.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly, RantPostUpdateDeletePermissions]
+    permission_classes = [IsAuthenticated, RantPostUpdateDeletePermissions]
 
