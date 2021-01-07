835baf29c0aaa2c6cf31344f56ddfbe02be538dc
anuraj <anurajguha@gmail.com>
2021-01-08 02:23:50 +0530

    add view to export user posts as list of lists

--- reportapp/urls.py
+++ reportapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.export_rants)
+    path('', views.export_rants),
+    path('json/', views.export_rants_as_json)
 ]
--- reportapp/views.py
+++ reportapp/views.py
@@ -1,6 +1,8 @@
 import csv
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes, api_view
+from rest_framework import status
+from rest_framework.response import Response
 from django.http import HttpResponse
 
 from rant_post.models import RantPost
@@ -30,4 +32,28 @@ def export_rants(request):
         writer.writerow([ post.title, post.text, feelings_dict[post.feeling_level], post.created_on.date(), post.created_on.time() ])
 
     return response
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def export_rants_as_json(request):
+    """ return all rants posted by a user in a json format """
+    feelings_dict = {
+        'S': 'Sad',
+        'VS': 'Very Sad',
+        'N': 'Neutral',
+        'P': 'Pissed',
+        'EP': 'Extremely Pissed',
+        'FF': 'Fucking Furious'
+    }
+    
+    posts = RantPost.objects.filter(author=request.user)
+
+    response_json = []
+    response_json.append(['Title', 'Body', 'Feeling Level', 'Created Date', 'Created Time'])
+    for post in posts:
+        response_json.append([ post.title, post.text, feelings_dict[post.feeling_level], post.created_on.date(), post.created_on.time() ])
+
+    return Response(response_json, status=status.HTTP_200_OK)
 
