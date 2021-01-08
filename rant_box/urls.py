64f5bac4421cda280e817657e160383753526d3f
anuraj <anurajguha@gmail.com>
2021-01-09 01:01:12 +0530

    add Custom Login view

--- rant_box/urls.py
+++ rant_box/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from .views import CustomAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rant-posts/', include('rant_post.urls')),
-    path('login/', views.obtain_auth_token),
+    path('login/', CustomAuthToken.as_view()),
     path('signup/', include('signup.urls')),
     path('generate-report/', include('reportapp.urls'))
 ]
