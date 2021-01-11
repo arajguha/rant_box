f7261aba46fd98cf0ebe491cd7a25b254eb8a88c
anuraj <anurajguha@gmail.com>
2021-01-12 00:45:50 +0530

    connect with  postgresql db

--- rant_box/settings.py
+++ rant_box/settings.py
@@ -87,14 +87,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rant_box.wsgi.application'
 
+# 'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+# }
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_TABLE'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD')
     }
 }
 
