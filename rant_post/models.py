6a2c5cc8d7df2f0da1f14a51767118efd3ce94a1
anuraj <anurajguha@gmail.com>
2021-02-07 01:07:06 +0530

    fix model error

--- rant_post/models.py
+++ rant_post/models.py
@@ -44,7 +44,7 @@ class PostReact(models.Model):
 
 
 class CategoryOption(models.Model):
-    category_name = models.CharField(max_length=20)
+    category_name = models.CharField(unique=True, max_length=20)
 
     def __str__(self):
-        return category_name
+        return self.category_name
